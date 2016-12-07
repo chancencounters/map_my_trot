28b11e0540edad5b2c3d08ee275fb7c028a1a28f
Zack Yu <zack.yu2@gmail.com>
2016-12-06 21:27:16 -0500

    Add map and route components

--- frontend/components/routes/create_routes_container.js
+++ frontend/components/routes/create_routes_container.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
 
+const mapStateToProps = (store) => {
+
+};
+
+const mapDispatchToProps = (store) => {
+
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateRoutes);
