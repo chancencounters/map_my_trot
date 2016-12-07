0354aed27ec3234402028a2598487c30d2099943
Zack Yu <zack.yu2@gmail.com>
2016-12-06 20:32:04 -0500

    Add routes controller and model

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: routes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  name       :string           not null
+#  distance   :string           not null
+#  map_info   :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Route < ApplicationRecord
+  validates :user, :name, :distance, :map_info
 end
