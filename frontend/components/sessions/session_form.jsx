f4f0059067b131bf934d15acc4320072e3340c89
Zack Yu <zack.yu2@gmail.com>
2016-12-08 09:45:58 -0500

    Refactor session form

--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -12,6 +12,8 @@ class SessionForm extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleGuestLogin = this.handleGuestLogin.bind(this);
+    this.renderLogInForm = this.renderLogInForm.bind(this);
+    this.renderSignUpForm = this.renderSignUpForm.bind(this);
   }
 
   handleSubmit(e) {
@@ -25,23 +27,6 @@ class SessionForm extends React.Component {
 			[field]: e.currentTarget.value
 		});
 	}
-
-  renderNameInputBoxes(formType) {
-    if (formType === 'signup') {
-      return (
-        <div>
-          <input type="text"
-            value={ this.state.first_name }
-            placeholder="First Name"
-            onChange={ this.update("first_name") }/>
-          <input type="text"
-            value={ this.state.last_name }
-            placeholder="Last Name"
-            onChange={ this.update("last_name") }/>
-        </div>
-      );
-    }
-  }
 
   handleGuestLogin(e) {
     e.preventDefault();
@@ -60,22 +45,69 @@ class SessionForm extends React.Component {
 		);
 	}
 
-  render () {
-    const { formType, errors } = this.props;
-
-    let link, button, error;
-    if (formType === 'login') {
-      link = <Link to="/signup" onClick={ this.props.clearErrors }>
-        New to MapMyTrot? Join Now
-      </Link>;
-      button = "LOG IN";
-    } else {
-      link = <Link to="/login" onClick={ this.props.clearErrors }>
-        Already have an account? Login
-      </Link>;
-      button = "JOIN NOW";
+  renderSignUpForm() {
+    if (this.props.formType === 'signup') {
+      return (
+        <div className="session_form_body">
+          <form onSubmit={ this.handleSubmit }>
+            <input type="text"
+              value={ this.state.first_name }
+              placeholder="First Name"
+              onChange={ this.update("first_name") }/>
+            <input type="text"
+              value={ this.state.last_name }
+              placeholder="Last Name"
+              onChange={ this.update("last_name") }/>
+            <input type="text"
+              value={ this.state.email }
+              placeholder="Email"
+              onChange={ this.update("email") }/>
+            <input type="password"
+              value={ this.state.password }
+              placeholder="Password"
+              onChange={ this.update("password") }/>
+            { this.renderErrors() }
+            <input type="submit"
+              onClick={ this.handleSubmit }
+              value="JOIN NOW"/>
+            <button onClick={ this.handleGuestLogin }>GUEST</button>
+            <Link to="/login" onClick={ this.props.clearErrors }>
+              Already have an account? Login
+            </Link>
+          </form>
+        </div>
+      );
     }
+  }
 
+  renderLogInForm() {
+    if (this.props.formType === 'login') {
+      return (
+        <div className="session_form_body">
+          <form onSubmit={ this.handleSubmit }>
+            <input type="text"
+              value={ this.state.email }
+              placeholder="Email"
+              onChange={ this.update("email") }/>
+            <input type="password"
+              value={ this.state.password }
+              placeholder="Password"
+              onChange={ this.update("password") }/>
+            { this.renderErrors() }
+            <input type="submit"
+              onClick={ this.handleSubmit }
+              value="LOG IN"/>
+            <button onClick={ this.handleGuestLogin }>GUEST</button>
+            <Link to="/signup" onClick={ this.props.clearErrors }>
+              New to MapMyTrot? Join Now
+            </Link>
+          </form>
+        </div>
+      );
+    }
+  }
+
+  render () {
     return (
       <div className="session_form_container">
         <header className="session_form_header">
@@ -84,27 +116,8 @@ class SessionForm extends React.Component {
             <h1>mapmytrot</h1>
           </div>
         </header>
-
-        <div className="session_form_body">
-          <form onSubmit={ this.handleSubmit }>
-            { this.renderNameInputBoxes(formType) }
-            <input type="text"
-  						value={ this.state.email }
-              placeholder="Email"
-  						onChange={ this.update("email") }/>
-            <input type="password"
-							value={ this.state.password }
-              placeholder="Password"
-							onChange={ this.update("password") }/>
-            { this.renderErrors() }
-            <input type="submit"
-              onClick={ this.handleSubmit }
-              value={ button }/>
-            <button onClick={ this.handleGuestLogin }>GUEST</button>
-            { link }
-          </form>
-        </div>
-
+        { this.renderLogInForm() }
+        { this.renderSignUpForm() }
       </div>
     );
   }
