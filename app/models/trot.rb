107766add3f339fc0d3856602d2a8845bb4a0a51
Zack Yu <zack.yu2@gmail.com>
2016-12-10 12:30:30 -0500

    Add Trots table

---
+++ app/models/trot.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: trots
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  route_id    :integer          not null
+#  name        :string           not null
+#  description :string           not null
+#  date        :date             not null
+#  duration    :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Trot < ApplicationRecord
+end
