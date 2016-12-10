a67fcf60808111edf4e7761ce5b0306e3282c197
Zack Yu <zack.yu2@gmail.com>
2016-12-10 12:55:23 -0500

    Add trot views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :routes, only: [:index, :show, :create, :destroy]
+    resources :trots, only: [:index, :show, :create, :destroy]
   end
 end
