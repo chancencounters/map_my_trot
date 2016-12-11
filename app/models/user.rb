8a14b158f7c1ea18871273f1c0da67b27b5d9b82
Zack Yu <zack.yu2@gmail.com>
2016-12-11 15:45:42 -0500

    Add Friendships Model and Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, default_url: "default_prof_pic.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  has_many :friends, -> { Friendship.accepted }, through: :friendships
   has_many :routes
   has_many :trots
 
