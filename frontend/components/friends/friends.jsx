1de09f6c43f6ebc3aa044be07ba28e0d5c610af0
Zack Yu <zack.yu2@gmail.com>
2016-12-11 21:31:05 -0500

    Add favicon and friends tabs

--- frontend/components/friends/friends.jsx
+++ frontend/components/friends/friends.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router';
+import FriendsProfileTabs from './friends_profile_tabs';
 
 class Friends extends React.Component {
   constructor(props) {
@@ -11,8 +13,11 @@ class Friends extends React.Component {
   }
 
   render() {
+    const { location } = this.props;
     return (
-      <div>
+      <div className="friends_container">
+        <h2>MY FRIENDS</h2>
+        <FriendsProfileTabs location={ location.pathname } />
       </div>
     );
   }
