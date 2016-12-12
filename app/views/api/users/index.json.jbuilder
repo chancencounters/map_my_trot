e6bd2f3b4c77fb9224c4c6aeb8a46eae6bd7dbc2
Zack Yu <zack.yu2@gmail.com>
2016-12-12 18:47:18 -0500

    AutoSearch friends

--- app/views/api/users/index.json.jbuilder
+++ app/views/api/users/index.json.jbuilder
@@ -2,7 +2,8 @@
   json.set! user.id do
     json.id user.id
     json.email user.email
-    json.name "#{user.first_name} #{user.last_name}"
+    json.first_name user.first_name
+    json.last_name user.last_name
     json.image_url asset_path(user.avatar.url)
   end
 end
--- frontend/components/friends/find_friends.jsx
+++ frontend/components/friends/find_friends.jsx
@@ -5,31 +5,33 @@ class FindFriends extends React.Component {
   constructor(props) {
     super(props);
 
+    this.searchVal = "";
     this.state = {
-      searchVal: "",
-      toggleSearch: false,
+      toggleSearchResults: false,
     };
     this.handleInput = this.handleInput.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.handleAddFriend = this.handleAddFriend.bind(this);
+    this.renderSearchResults = this.renderSearchResults.bind(this);
   }
 
   matches() {
     const matches = [];
-    const { searchVal } = this.state;
     const { potentialFriends } = this.props;
 
-    if (searchVal.length === 0) {
+    if (this.searchVal.length === 0) {
       return potentialFriends;
     }
 
     potentialFriends.forEach(potentialFriend => {
-      let nameSub = potentialFriend.name.slice(0, searchVal.length);
-      let emailSub = potentialFriend.email.slice(0, searchVal.length);
+      let firstNameSub = potentialFriend.first_name.slice(0, this.searchVal.length);
+      let lastNameSub = potentialFriend.last_name.slice(0, this.searchVal.length);
+      let emailSub = potentialFriend.email.slice(0, this.searchVal.length);
 
       if (
-        nameSub.toLowerCase() === searchVal.toLowerCase() ||
-        emailSub.toLowerCase() === searchVal.toLowerCase()
+        firstNameSub.toLowerCase() === this.searchVal.toLowerCase() ||
+        lastNameSub.toLowerCase() === this.searchVal.toLowerCase() ||
+        emailSub.toLowerCase() === this.searchVal.toLowerCase()
       ) {
         matches.push(potentialFriend);
       }
@@ -38,8 +40,32 @@ class FindFriends extends React.Component {
     return matches;
   }
 
+  renderSearchResults() {
+    debugger
+    if (this.state.toggleSearchResults) {
+      return (
+        <ul className="find_friends_list">
+          { this.matches().map((user) => {
+            return (
+              <li className="find_friends_list_item" key={ user.id }>
+                <img src={ user.image_url}/>
+                <div className="find_friends_inner">
+                  <span>{ user.first_name + " " + user.last_name }</span>
+                  <span>{ user.email }</span>
+                  <div onClick={ () => this.handleApproval(user.id) }>Add</div>
+                </div>
+              </li>
+            );
+          })
+        }
+      </ul>
+    );
+    }
+  }
+
   handleInput(e) {
-    this.setState({ searchVal: e.currentTarget.value });
+    this.searchVal = e.currentTarget.value;
+    this.forceUpdate();
   }
 
   handleAddFriend(id) {
@@ -47,17 +73,18 @@ class FindFriends extends React.Component {
   }
 
   handleSearch() {
-    this.setState({ toggleSearch: !this.state.toggleSearch });
+    this.setState({ toggleSearchResults: true });
   }
 
   render() {
     const { users } = this.props;
+
     return (
       <div className="find_friends_container">
         <p>FIND MAPMYTROT FRIENDS BY NAME OR EMAIL:</p>
         <form className="find_friends_search_form">
           <input type="text"
-            value={ this.state.search }
+            value={ this.searchVal }
             onChange={ (e) => this.handleInput(e) }
           />
           <input type="submit"
@@ -65,21 +92,7 @@ class FindFriends extends React.Component {
             onClick={ this.handleSearch }
           />
         </form>
-        <ul className="find_friends_list">
-          { this.matches().map((user) => {
-            return (
-              <li className="find_friends_list_item" key={ user.id }>
-                <img src={ user.image_url}/>
-                <div className="find_friends_inner">
-                  <span>{ user.name }</span>
-                  <span>{ user.email }</span>
-                  <div onClick={ () => this.handleApproval(user.id) }>Add</div>
-                </div>
-              </li>
-            );
-          })
-          }
-        </ul>
+        { this.renderSearchResults() }
       </div>
     );
   }
