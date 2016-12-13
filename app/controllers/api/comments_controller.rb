0b01c5f0b69fe2483fbcd7cd930aaf81af966aff
Zack Yu <zack.yu2@gmail.com>
2016-12-13 14:23:04 -0500

    Fix create method for comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = current_user.comments.new(comment_params)
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user_id = current_user.id
 
     if @comment.save
       render json: @comment
@@ -10,7 +12,20 @@ class CommentsController < ApplicationController
     end
   end
 
+  def create
+
+  end
+
   private
+  def find_commentable
+    params.each do |name, value|
+      if name =~ /(.+)_id$/
+        return $1.classify.constantize.find(value)
+      end
+    end
+    nil
+  end
+
   def comment_params
     params.require(:comment).permit(:body, :commentable_id, :commentable_type)
   end
