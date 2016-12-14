68b1255ff368c48fe8861f6c8dfb8987bc12582b
Zack Yu <zack.yu2@gmail.com>
2016-12-14 09:37:31 -0500

    Add Dashboard Container

--- frontend/components/home/dashboard/dashboard_container.js
+++ frontend/components/home/dashboard/dashboard_container.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { asArray } from "../../../reducers/selectors";
 import Dashboard from './dashboard';
 import { postRoute } from '../../../actions/route_actions';
 import { clearErrors } from '../../../actions/error_actions';
 
 const mapStateToProps = (store) => ({
   currentUser: store.session.currentUser,
+  trots: asArray(store.trots),
 });
 
 const mapDispatchToProps = (dispatch) => ({
