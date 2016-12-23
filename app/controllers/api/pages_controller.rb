c6f32f3bfa884d42e6ea17e10954e6fe2ca58663
Zack Yu <zack.yu2@gmail.com>
2016-12-23 16:43:34 -0500

    Add new file

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
     def letsencrypt
-      render text: "OvvdoQal8_LTgx3yd1fZqkY8RvFauYVjGjfhMo9l140.ZHXbcVkvpyi0Id63T2Q43S5Wc8VwNfygPvnFq8m8yUc"
+      render text: "-rLWjngGFTmQ_XYyMll5qbqLn7zH9pjXbcDYohsSxlU.ZHXbcVkvpyi0Id63T2Q43S5Wc8VwNfygPvnFq8m8yUc"
     end
   end
