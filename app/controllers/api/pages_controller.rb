47d800108c197f930e6740d13351c367680f7be9
Zack Yu <zack.yu2@gmail.com>
2017-01-04 09:38:17 -0500

    Add SSL Cert

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
     def letsencrypt
-      render text: "l2l2wM5IqvNLPdvZoNxLsJm2PaUk5orpOzId1MuIBJA.RiHaqv0tt26bI_37Xz68926qC2sXxsP50qq0iJF4l_A"
+      render text: "u3x5Gl4vhhRaXSkwKKlzCUSYhmMgYD2yNiDucUNSDk8.RiHaqv0tt26bI_37Xz68926qC2sXxsP50qq0iJF4l_A"
     end
   end
