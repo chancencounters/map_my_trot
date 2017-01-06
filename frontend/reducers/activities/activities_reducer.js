36756b647074c17d035a539a8fa760600cd23cc5
Zack Yu <zack.yu2@gmail.com>
2017-01-06 15:37:41 -0500

    Allow activities to receive current user with updated avatar

--- frontend/reducers/activities/activities_reducer.js
+++ frontend/reducers/activities/activities_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_ACTIVITIES } from '../../actions/activity_actions';
+import { RECEIVE_CURRENT_USER } from '../../actions/session_actions';
 import { RECEIVE_NEW_COMMENT, REMOVE_COMMENT } from '../../actions/comment_actions';
 import { removeComment, receiveNewComment } from '../selectors';
 
@@ -8,6 +9,18 @@ const ActivitiesReducer = (state = {}, action) => {
   switch(action.type) {
     case RECEIVE_ACTIVITIES:
       return action.activities;
+    case RECEIVE_CURRENT_USER:
+      let activities = {};
+      Object.keys(state).forEach((key) => {
+        let activity = state[key];
+        if (activity.user.id === action.currentUser.id) {
+          let newActivity = Object.assign({}, activity, { user: action.currentUser });
+          activities = Object.assign(activities, { [key]: newActivity} );
+        } else {
+          activities = Object.assign(activities, { [key]: activity});
+        }
+      });
+      return activities;
     case RECEIVE_NEW_COMMENT:
       return receiveNewComment(state, action.comment);
     case REMOVE_COMMENT:
