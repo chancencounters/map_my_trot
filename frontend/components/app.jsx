8f2e396c0536f55b8c3389941abd381be19ee041
Zack Yu <zack.yu2@gmail.com>
2017-02-16 11:46:02 -0500

    Fix header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeaderContainer from './headers/header_container';
 import Footer from './footer';
 
 const App = ({ children }) => (
   <div className="main_body_container">
+    <HeaderContainer />
     { children }
   </div>
 );
